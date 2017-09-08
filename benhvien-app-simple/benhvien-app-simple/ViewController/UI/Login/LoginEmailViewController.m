71797beb3adbbbc4dcde54fd079d36708f524ec7
PhucNT <ntp201094@gmail.com>
2017-09-08 21:18:49 +0700

    no message

--- benhvien-app-simple/benhvien-app-simple/ViewController/UI/Login/LoginEmailViewController.m
+++ benhvien-app-simple/benhvien-app-simple/ViewController/UI/Login/LoginEmailViewController.m
@@ -7,6 +7,13 @@
 //
 
 #import "LoginEmailViewController.h"
+#import "UIAlertController+Blocks.h"
+#import "AppDelegate.h"
+
+typedef enum : NSUInteger {
+  LOGIN = 0,
+  REGISTER
+} Screen;
 
 @interface LoginEmailViewController ()
 
@@ -20,6 +27,8 @@
 @property (weak, nonatomic) IBOutlet UITextField *signUpPasswordTextField;
 @property (weak, nonatomic) IBOutlet UITextField *signUpCityTextField;
 
+@property (nonatomic, assign) Screen currentScreen;
+
 
 @end
 
@@ -28,6 +37,7 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
   [self setupNavigationBarItem];
+  self.currentScreen = LOGIN;
 }
 
 - (void)setupNavigationBarItem {
@@ -43,11 +53,83 @@
 }
 
 - (void)cancelLoginScreen {
-  [self dismissViewControllerAnimated:true completion:nil];
+//  UIAlertController *cancelConfirmingAlertController = [UIAlertController alertControllerWithTitle:@"Xác nhận" message:@"Bạn có chắc chắn muốn hủy bỏ?" preferredStyle:UIAlertControllerStyleAlert];
+//  UIAlertAction *yesAction = [UIAlertAction actionWithTitle:@"Yes" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+//    [self dismissViewControllerAnimated:YES completion:nil];
+//  }];
+//  UIAlertAction *noAction = [UIAlertAction actionWithTitle:@"No" style:UIAlertActionStyleCancel handler:nil];
+//  
+//  [cancelConfirmingAlertController addAction:yesAction];
+//  [cancelConfirmingAlertController addAction:noAction];
+//  [self presentViewController:cancelConfirmingAlertController animated:YES completion:nil];
+  
+  [UIAlertController showAlertInViewController:self withTitle:@"Xác nhận" message:@"Bạn có chắc chắn muốn hủy bỏ?" cancelButtonTitle:@"No" destructiveButtonTitle:@"Yes" otherButtonTitles:nil tapBlock:^(UIAlertController * _Nonnull controller, UIAlertAction * _Nonnull action, NSInteger buttonIndex) {
+    if (buttonIndex == 1) {
+      [self dismissViewControllerAnimated:YES completion:nil];
+    }
+  }];
 }
 
 - (void)doneLoginScreen {
+  [self.view endEditing:YES];
+  switch (self.currentScreen) {
+    case LOGIN:{
+      [self loginUser];
+      break;
+    }
+    case REGISTER:{
+      
+      break;
+    }
+      
+    default:
+      break;
+  }
+}
+
+- (void)loginUser {
+  NSString *email = self.signInUserNameTextField.text;
+  NSString *password = self.signInPasswordTextField.text;
+  [self validateEmail:email password:password completion:^(NSString *message, BOOL isValid) {
+    if (isValid) {
+      [self loginWithEmail:email password:password];
+    } else {
+      [self showAlertWithTitle:@"Lỗi" message:message];
+    }
+  }];
+}
+
+- (void)registerUser {
   
+}
+
+- (void)validateEmail:(NSString *)email password:(NSString *)password completion:(void (^)(NSString *message, BOOL isValid))completion {
+  if (!email || email.length == 0) {
+    completion(@"Bạn phải nhập email!", NO);
+    return;
+  }
+  if (!password || password.length == 0) {
+    completion(@"Bạn phải nhập password!", NO);
+    return;
+  }
+  completion(@"", YES);
+}
+
+- (void)loginWithEmail:(NSString *)email password:(NSString *)password {
+  [self showHUD];
+  [ApiRequest loginWithEmail:email password:password completionBlock:^(ApiResponse *response, NSError *error) {
+    [self hideHUD];
+    if (!error) {
+      if (response.success) {
+        AppDelegate *app = (AppDelegate *)[[UIApplication sharedApplication] delegate];
+        [app setupHomeScreen];
+      } else {
+        [self showAlertWithTitle:@"Lỗi" message:response.message];
+      }
+    } else {
+      NSLog(@"%@", [error localizedDescription]);
+    }
+  }];
 }
 
 - (void)displaySignInView {
@@ -72,10 +154,7 @@
 }
 
 -(void)resetSignUpViewInput {
-  [self.signUpFullNameTextField endEditing:YES];
-  [self.signUpEmailTextField endEditing:YES];
-  [self.signUpPasswordTextField endEditing:YES];
-  [self.signUpCityTextField endEditing:YES];
+  [self.view endEditing:YES];
   self.signUpFullNameTextField.text = @"";
   self.signUpEmailTextField.text = @"";
   self.signUpPasswordTextField.text = @"";
@@ -84,10 +163,10 @@
 
 - (IBAction)changeSegmentTab:(UISegmentedControl *)sender {
   switch (sender.selectedSegmentIndex) {
-    case 0:
+    case LOGIN:
       [self displaySignInView];
       break;
-    case 1:
+    case REGISTER:
       [self displaySignUpView];
       break;
       

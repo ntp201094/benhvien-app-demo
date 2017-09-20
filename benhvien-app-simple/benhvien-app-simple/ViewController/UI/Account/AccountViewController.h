ba9af2c1af409af1b81c7955df3c2de26a6399f0
PhucNT <ntp201094@gmail.com>
2017-09-20 21:08:57 +0700

    no message

---
+++ benhvien-app-simple/benhvien-app-simple/ViewController/UI/Account/AccountViewController.h
@@ -0,0 +1,13 @@
+//
+//  AccountViewController.h
+//  benhvien-app-simple
+//
+//  Created by test on 9/20/17.
+//  Copyright © 2017 test. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface AccountViewController : BaseViewController
+
+@end
---
+++ benhvien-app-simple/benhvien-app-simple/ViewController/UI/Account/ChangePasswordViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChangePasswordViewController.h
+//  benhvien-app-simple
+//
+//  Created by test on 9/20/17.
+//  Copyright © 2017 test. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface ChangePasswordViewController : BaseViewController
+
+@end
